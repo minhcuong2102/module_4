68f866130cc4a8fd78c9bdb2256e7b0f172fba7a
minhcuong2102 <cuong21022003@gmail.com>
2023-03-22 18:06:13 +0700

    update

---
+++ bai_13/bai_tap/quan_ly_blog/src/test/java/com/codegym/quan_ly_blog/QuanLyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.quan_ly_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLyBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
